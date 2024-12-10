391461de36d1ea4be9cc9a11a9fddf51f665185c
Angel Vasquez <angelvasquez199912@icloud.com>
2024-12-09 22:43:21 -0600

    modificaciones en el navbar

--- js/codigo_final.js
+++ js/codigo_final.js
@@ -1,7 +1,1 @@
-<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-<li><a class="dropdown-item" href="#"><i class="bi bi-person-arms-up"></i>Yoga</a></li>
-<li><a class="dropdown-item" href="#"><i class="bi bi-person-walking"></i>Caminadoras</a></li>
-<li><a class="dropdown-item" href="#"><i class="fa-solid fa-dumbbell"></i>Pesas</a></li>
-<ul class="dropdown-divider"></ul>
-<li><a class="dropdown-item" href="#">Y mas</a></li>
-</ul>
+         
