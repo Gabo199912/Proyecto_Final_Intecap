4cd0ade1efa1898ba44aba0c301d8b4190fb34ea
Angel Vasquez <angelvasquez199912@icloud.com>
2024-12-15 23:07:13 -0600

    ingreso los cambios derivados a los planes, tambien un video como prueba

--- js/codigo_final.js
+++ js/codigo_final.js
@@ -1,10 +1,29 @@
  window.sr = ScrollReveal();
  
- 
 
- sr.reveal('.banner', {
+
+ sr.reveal('.carousel-inner', {
     duration: 3000,
     origin: 'bottom',
     distance: '-100px'
 });
+
+sr.reveal('.carta-1', {
+    duration: 3000,
+    origin: 'right',
+    distance: '-100px'
+});
+
+sr.reveal('.carta-2', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-100px'
+});
+
+sr.reveal('.carta-3', {
+    duration: 3000,
+    origin: 'left',
+    distance: '-100px'
+});
+
 
